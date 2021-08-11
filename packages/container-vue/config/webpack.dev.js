3031bbb04b730769fa6ae60742a2bcfa565ee314
nikhil <nikhil.gosavi@pentacor.de>
2021-08-11 09:27:58 +0200

    changes

---
+++ packages/container-vue/config/webpack.dev.js
@@ -0,0 +1,32 @@
+const { merge } = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const devConfig = {
+  mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8080/',
+  },
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        productlist: 'productlist@http://localhost:8083/remoteEntry.js',
+      },
+      shared: packageJson.dependencies,
+    }),
+
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig);
---
+++ packages/container-vue/src/bootstrap.js
@@ -0,0 +1,12 @@
+
+import Vue, { createApp } from 'vue';
+import App from "./App.vue";
+import { mount } from 'productlist/ProductList';
+
+// import VueRouter from 'vue-router'
+
+// Vue.use(VueRouter)
+
+// const app = createApp(App);
+// app.mount('#root');
+mount('#root')
